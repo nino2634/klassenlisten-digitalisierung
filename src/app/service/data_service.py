35486a0c2cf530816289109deaa7435c348e432e
nino2634 <niklas.noechel@gmx.de>
2025-09-18 10:28:00 +0200

    read excel file

--- src/app/service/data_service.py
+++ src/app/service/data_service.py
@@ -1,4 +1,18 @@
 import csv
+import openpyxl
 
-with open("data.csv", newline="") as csvfile:
-    
+file_path = "/home/niklas/Coding/klassenlisten-digitalisierung/src/app/data/Datenquelle.xlsx"
+
+def read_csv():
+    with open(file=file_path, newline="") as csvfile:
+        print(csvfile)
+
+def read_openpyxl():
+    wb = openpyxl.load_workbook(file_path)
+    sheet = wb.active
+
+    cell=sheet.cell(2, 1)
+
+    print(cell.value)
+
+read_openpyxl()
