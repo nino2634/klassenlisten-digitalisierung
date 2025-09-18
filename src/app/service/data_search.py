ab87d152f1666e6fcef729166a461735cd7c9e9d
Kurt722 <47476412+Flo722@users.noreply.github.com>
2025-09-18 11:48:19 +0200

    added data structure
    Added lessons class
    Started work on seach

---
+++ src/app/service/data_search.py
@@ -0,0 +1,4 @@
+from app.struc.lesson import Lesson
+
+def search_lessons(lessons: list[Lesson]) -> Lesson:
+	pass
---
+++ src/app/struc/lesson.py
@@ -0,0 +1,16 @@
+class Lesson:
+    # Constructor / initializer
+    def __init__(lv_id, subject, teacher, note, teacher_lessons, student_lessons, school_classes : list[str]):
+        # Instance attributes
+        self.lv_id = lv_id
+        self.subject = subject
+        self.teacher = teacher
+        self.note = note
+        self.teacher_lessons = teacher_lessons
+        self.student_lessons = student_lessons
+        self.school_classes = school_classes
+
+    #Wie das dahstehen soll ist noch offen erstmal nur auflistung
+    def create_notes():
+        for school_class in school_classes:
+            note = note + "/" + school_class
