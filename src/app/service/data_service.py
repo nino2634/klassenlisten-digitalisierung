9d8bac122af9e6dec63dcd31a8c0b148bb5817ec
nino2634 <niklas.noechel@gmx.de>
2025-09-18 11:50:42 +0200

    added read_excel_file

--- src/app/service/data_service.py
+++ src/app/service/data_service.py
@@ -1,18 +1,17 @@
-import csv
 import openpyxl
 
 file_path = "/home/niklas/Coding/klassenlisten-digitalisierung/src/app/data/Datenquelle.xlsx"
-
-def read_csv():
-    with open(file=file_path, newline="") as csvfile:
-        print(csvfile)
 
 def read_openpyxl():
     wb = openpyxl.load_workbook(file_path)
     sheet = wb.active
 
-    cell=sheet.cell(2, 1)
+    row_count = sheet.max_row
 
-    print(cell.value)
+    for row in range(row_count):
+        if row > 0:
+            cell=sheet.cell(row=row, column=1)
+
+            print(cell.value)
 
 read_openpyxl()
