1c25e5c79ec4dbf24f1f15619b78b6953f7231ee
nino2634 <niklas.noechel@gmx.de>
2025-10-23 10:50:22 +0200

    finished get_classes functionality

--- src/app/getclasses.py
+++ src/app/getclasses.py
@@ -1,21 +1,22 @@
-import sys
+from config_handler import Config_Handler
 import openpyxl
 
-try:
-    filter = sys.argv[1]
-except IndexError:
-    filter = ""
+def init_file():
+    data_file = Config_Handler()
+    file_path = data_file.load_data_file_path()
+    return file_path
 
-def get_classes_from_file(sheet, start_row): 
+def get_classes_from_file(sheet, start_row, class_filter): 
     classes = []
     spacer = 3 # empty rows between class name and lessons
-    end_row = int(sheet.max_row)
-    row:int = start_row
+    end_row = sheet.max_row
+    row = start_row
     while end_row > row:
         cell = sheet.cell(row=row, column=1)
         if cell.value is not None:
             print(cell.value)
-            classes.append(cell.value)
+            if(class_filter in cell.value):
+                classes.append(cell.value)
             if(row+spacer < end_row):
                 new_row=skip_lessons(sheet=sheet, start_row=row+spacer, end_row=end_row)
                 row = new_row
@@ -28,18 +29,19 @@ def get_classes_from_file(sheet, start_row):
 def skip_lessons(sheet, start_row, end_row):
     spacer = 1 # empty rows between lessons and next class name
     for row in range(start_row, end_row):
+        print(row)
         cell = sheet.cell(row=row, column=1)
-        #print(cell.value)
+        print(cell.value)
         if(cell.value is None and row != end_row):
             return (row + spacer)
-        else:
-            return end_row
+    return end_row
 
-def run():
-    filePath = "src/app/data/Datenquelle.xlsx"
+def run(class_filter = ""):
+    print(class_filter)
+    filePath = init_file()
     workbook = openpyxl.load_workbook(filePath)
     sheet=workbook.active
-    classes = get_classes_from_file(sheet=sheet, start_row=1)
+    classes = get_classes_from_file(sheet=sheet, start_row=1, class_filter=class_filter)
     print(classes)
 
 run()
