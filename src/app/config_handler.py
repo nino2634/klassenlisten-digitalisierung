8cb75df6990b0fdedeacc3e1f32c75510157228a
Kurt722 <47476412+Flo722@users.noreply.github.com>
2025-10-30 09:12:14 +0100

    changed get_path

--- src/app/config_handler.py
+++ src/app/config_handler.py
@@ -1,5 +1,13 @@
 import os
-from get_path import create_path
+
+
+#puts together the absolute path of the a file file 
+def create_path(path, file_name) -> str:
+    current_file = os.path.abspath(__file__)
+    project_root = os.path.dirname(os.path.dirname(current_file))
+    file = os.path.join(project_root, path, file_name)
+
+    return file
 
 #returns the path of the excel file
 def load_data_file_path() -> str:
