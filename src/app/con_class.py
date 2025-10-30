49bc7a952c61509f0e35017b3d6929559e609763
Kurt722 <47476412+Flo722@users.noreply.github.com>
2025-10-30 09:09:02 +0100

    fixed

--- src/app/con_class.py
+++ src/app/con_class.py
@@ -1,26 +1,14 @@
 import get_classes
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import get_headers
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 import sys
 import json
 
 #Main method for doing shit
 def run(_filter):
     class_list = get_classes.run(_filter)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     for class_i in class_list:
         print(class_i)
     class_json = json.dumps(class_list)
     return class_json
-=======
-=======
->>>>>>> Stashed changes
     print(json.dumps(class_list))
 
 #Only for testing
@@ -33,4 +21,4 @@ if __name__ == "__main__":
         run(sys.argv[1])
     else:
         print("No argument provided")
->>>>>>> Stashed changes
+
