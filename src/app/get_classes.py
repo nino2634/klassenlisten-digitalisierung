0284f3defd5c3bd3e203de9e1fa9fb4e00eedf4b
nino2634 <niklas.noechel@gmx.de>
2025-10-30 09:13:55 +0100

    added comments and config handler change

--- src/app/get_classes.py
+++ src/app/get_classes.py
@@ -1,12 +1,16 @@
-from config_handler import Config_Handler
+import config_handler as ch
 import openpyxl
 
-def init_file():
-    data_file = Config_Handler()
-    file_path = data_file.load_data_file_path()
+# gets file path from config_handler
+# returns path of file as String
+def _initiate_file():
+    file_path = ch.load_data_file_path()
     return file_path
 
-def get_classes_from_file(sheet, start_row, class_filter): 
+# searches all class names in excel file
+# if filter is set, searches classes with specific names
+# returns: class names as list
+def _get_classes_from_file(sheet, start_row, class_filter): 
     classes = []
     spacer = 3 # empty rows between class name and lessons
     end_row = sheet.max_row
@@ -17,7 +21,7 @@ def get_classes_from_file(sheet, start_row, class_filter):
             if(class_filter in cell.value):
                 classes.append(cell.value)
             if(row+spacer < end_row):
-                new_row=skip_lessons(sheet=sheet, start_row=row+spacer, end_row=end_row)
+                new_row=_skip_lessons(sheet=sheet, start_row=row+spacer, end_row=end_row)
                 row = new_row
             else:
                 break;
@@ -25,7 +29,9 @@ def get_classes_from_file(sheet, start_row, class_filter):
             row += 1
     return classes
 
-def skip_lessons(sheet, start_row, end_row):
+# skips all lessons to get to the next class name
+# returns: row where list of lessons end as Int
+def _skip_lessons(sheet, start_row, end_row):
     spacer = 1 # empty rows between lessons and next class name
     for row in range(start_row, end_row):
         cell = sheet.cell(row=row, column=1)
@@ -33,11 +39,13 @@ def skip_lessons(sheet, start_row, end_row):
             return (row + spacer)
     return end_row
 
+# executes functions
+# returns class names as list
 def run(class_filter = ""):
-    filePath = init_file()
+    filePath = _initiate_file()
     workbook = openpyxl.load_workbook(filePath)
     sheet=workbook.active
-    classes = get_classes_from_file(sheet=sheet, start_row=1, class_filter=class_filter)
+    classes = _get_classes_from_file(sheet=sheet, start_row=1, class_filter=class_filter)
     #print(classes)
     return classes
 
--- src/app/get_headers.py
+++ src/app/get_headers.py
@@ -1,17 +1,14 @@
 import openpyxl
-from config_handler import Config_Handler
-## ToDo
-## please add "_" to private classes
-## add descriptions for the other methods
+import config_handler as ch
 
 # gets file path from config_handler
-# returns: String
+# returns: file path as String
 def _initiate_file():
-    data_file = Config_Handler()
-    file_path = data_file.load_data_file_path()
+    file_path = ch.load_data_file_path()
     return file_path
 
-#reads all consecutive not-empty cells in a row
+# reads all consecutive not-empty cells in a row
+# returns: column names as list
 def _read_columns_in_row(sheet, row) -> list:
     values = []
     for column in range(1, 100):
@@ -23,7 +20,7 @@ def _read_columns_in_row(sheet, row) -> list:
     return values
 
 # reads all headers from excel file
-# returns: list 
+# returns: header names as list
 def _get_headers(sheet, title_row):
     headers = _read_columns_in_row(sheet=sheet, row=title_row + 3) # wir schauen hier nochmal in den großen Listen, was der Abstand ist, 3 Zielen im Testding
     list = []
@@ -43,7 +40,7 @@ def _read_excel_file(sheet, start_row):
             return headers
 
 # executes functions
-# returns: list
+# returns: header names as list
 def run():
     file_path = _initiate_file()
     workbook = openpyxl.load_workbook(file_path)
