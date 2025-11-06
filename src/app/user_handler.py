541a7ab1d66beafb9af713efd2647922156991b2
nino2634 <niklas.noechel@gmx.de>
2025-11-06 10:18:19 +0100

    testing and renaming

--- src/app/user_handler.py
+++ src/app/user_handler.py
@@ -37,4 +37,4 @@ def verify_user(user,hash):
         return
 
 #Debug
-run("LUST","0d3253c203057b5728f73d7b28783ef55211511f4d190620204bccb8f7b59671")
+#run("LUST","0d3253c203057b5728f73d7b28783ef55211511f4d190620204bccb8f7b59671")
