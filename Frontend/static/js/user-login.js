67d540a8ea335f42bdf612ed588aae6738cba1fa
Privatacc <privatacc77@proton.me>
2025-11-13 09:28:27 +0100

    changed user-login.js

--- Frontend/static/js/user-login.js
+++ Frontend/static/js/user-login.js
@@ -8,8 +8,8 @@ try {
     const user = document.getElementById('user').innerHTML
     const password = document.getElementById('pass').innerHTML
     const response = await fetch('http://10.49.128.174:5000/api/verify_user?user='
-    + encodeURIComponent(userTMP)
-    +'&password='+ encodeURIComponent(passwordTMP)); 
+    + encodeURIComponent(user)
+    +'&password='+ encodeURIComponent(password));
     const data = await response.text();
     console.log(data);
 } catch (error) {
