30c3c8b4f3c798dc565f77b179415af33ea1aa4e
Kurt722 <47476412+Flo722@users.noreply.github.com>
2025-11-20 09:25:21 +0100

    change the config handler

--- src/app/con_lesson.py
+++
@@ -1,9 +0,0 @@
-
-def run(class_name):
-    #class_list = run(class_name)
-    #for class_i in class_list:
-        #print(class_i)
-    pass
-
-if __name__ == "__main__":
-    run()
---
+++ src/app/config/config_handler.py
@@ -0,0 +1,35 @@
+import os
+
+#puts together the absolute path of the a file file
+#can be used in other .py to make absolut paths for files
+def create_path(path, file_name) -> str:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, path, file_name)
+
+#-----------------------------------------------------------
+#Returns the matching value from the config
+#-----------------------------------------------------------
+def load_data_file_path(value_name : str) -> str:
+    settings_path = create_path("", "settings.txt")
+
+    if not os.path.exists(settings_path):
+        print("Error: settings.txt not found.")
+        return
+    else:
+        with open(settings_path) as f:
+            lines = f.readlines()
+    print(lines)
+    value = ""
+
+    for line in lines:
+        print(line)
+        if line.startswith(value_name + "="):
+            value = line.split('=', 1)[1].strip()
+            break
+
+    if value == "":
+        print("Error: No value matching key: " + value_name)
+        return
+
+    return value
+
