52cac2e8c589ee5c6f86acae59b7ef9d1d6c0f85
nino2634 <niklas.noechel@gmx.de>
2025-12-18 09:57:25 +0100

    reworked functionality

--- src/app/get_lessons.py
+++ src/app/get_lessons.py
@@ -17,64 +17,92 @@ def _find_class_title_row(sheet, class_filter):
         if cell.value == class_filter:
             return row
     raise Exception("Class not found")
-
-def add_sums(lesson_list):
-    sum_sus = 0
-    sum_kuk = 0
-    
-    for lesson in lesson_list[0]['lessons']:
-        sum_sus += int(lesson['WoStd_SuS'])
-        sum_kuk += int(lesson['WoStd_KuK'])
-    
-def _get_lessons_by_class(sheet, class_title, year_half, headers):
+        
+def get_lessons_from_sheet(sheet, headers, class_title):
     title_row = _find_class_title_row(sheet, class_title)
     start_row = get_next_row_with_value(sheet, title_row + 1) + 1
     end_row = get_next_empty_row(sheet, start_row)
-
+    
     start_col = sheet.min_column
     end_col = sheet.max_column
     lesson_list = []
-    list = []
+    
     for row in range(start_row, end_row):
         lesson = {}
         for col in range(start_col, end_col):
             cell = sheet.cell(row, col)
-            header = headers[col-1]
-            lesson[header] = f"{cell.value}"
-            
-        if "," in lesson[classes_column_name]:
-            classes = str.split(lesson[classes_column_name], ",")
-            comment = "gekoppelt mit"
-            for class_name in classes:
-                if class_name != class_title:
-                    comment += f" {class_name},"
-            lesson['comment'] = comment[:len(comment) - 1]
-        if year_half in lesson[half_year_column_name]:
-            list.append(lesson)
+            if headers[col-1] == f"{weekly_hrs_column_name}_SuS":
+                try:
+                    lesson[headers[col-1]] = int(cell.value)
+                except(TypeError):
+                    lesson[headers[col-1]] = 0
+            elif headers[col-1] == f"{weekly_hrs_column_name}_KuK":
+                try:
+                    lesson[headers[col-1]] = float(cell.value)
+                except(TypeError):
+                    lesson[headers[col-1]] = 0
+            else:
+                lesson[headers[col-1]] = f"{cell.value}"
+        if lesson[f"{weekly_hrs_column_name}_SuS"] == "None":
+            lesson[f"{weekly_hrs_column_name}_SuS"] = 0
+        if lesson[f"{weekly_hrs_column_name}_KuK"] == "None":
+            lesson[f"{weekly_hrs_column_name}_KuK"] = 0
+        lesson_list.append(lesson)
+    return lesson_list
     
+def process_lessons(class_title, contrary_year_half, lessons):
+    list = []
     sum_sus = 0
     sum_kuk = 0
     
-    for lesson in list:
-        #print(lesson['comment'])
-        if lesson[f"{weekly_hrs_column_name}_SuS"] !='None':
+    try:
+        for i in range(0, len(lessons)):
+            lesson = lessons[i]
+            try:
+                if contrary_year_half not in lesson[half_year_column_name]:
+                    if "," in lesson[classes_column_name]:
+                        classes = str.split(lesson[classes_column_name], ",")
+                        comment = "gekoppelt mit"
+                        for class_name in classes:
+                            if class_name != class_title:
+                                comment += f" {class_name},"
+                        lesson['comment'] = comment[:len(comment) - 1]
+
+                        while lesson['Fach'] == lessons[i + 1]['Fach'] and lesson[half_year_column_name] == lessons[i+1][half_year_column_name]:
+                            if lessons[i+1][f"{weekly_hrs_column_name}_SuS"] != "None":
+                                lesson[f"{weekly_hrs_column_name}_SuS"] += int(lessons[i+1][f"{weekly_hrs_column_name}_SuS"])
+                            if lessons[i+1][f"{weekly_hrs_column_name}_KuK"] != "None":
+                                lesson[f"{weekly_hrs_column_name}_KuK"] += float(lessons[i+1][f"{weekly_hrs_column_name}_KuK"])
+                            lessons.pop(i+1)
+            except(IndexError): 
+                pass      
             sum_sus += int(lesson[f"{weekly_hrs_column_name}_SuS"])
-        if lesson[f"{weekly_hrs_column_name}_KuK"] != 'None':
             sum_kuk += float(lesson[f"{weekly_hrs_column_name}_KuK"])
+                    
+            list.append(lesson)
+    except(IndexError):
+        pass # basically ignore and continue
     
     sum_sus = round(sum_sus*100)/100
-    sum_kuk = round(sum_kuk*100)/100
+    sum_kuk = round(sum_kuk*100)/100 
     
-    lesson_list.append({"class_name": f"{class_title}", "lessons": list, "Sum_SuS": sum_sus, "Sum_KuK": sum_kuk})
-    return lesson_list
+    return {"class_name": f"{class_title}", "lessons": list, "Sum_SuS": sum_sus, "Sum_KuK": sum_kuk}
 
 def run(class_title, year_half):
+    contrary_year_half = year_half
+    if "1" in year_half:
+        contrary_year_half= str.replace(contrary_year_half, "1", "2")
+    elif "2" in year_half:
+        contrary_year_half= str.replace(contrary_year_half, "2", "1")
+    
     workbook = initiate_file()
     sheet = workbook.active
+    
     headers = json.loads(get_headers())
-    lesson_list = _get_lessons_by_class(sheet, class_title, year_half, headers)  
-    lesson_json = json.dumps(lesson_list)  
+    lessons = get_lessons_from_sheet(sheet, headers, class_title)
+    lesson_list = process_lessons(class_title, contrary_year_half, lessons)
+    lesson_json = json.dumps(lesson_list)
     return lesson_json
 
-#result = run("02TSFR", "1.Hj")
-#print(result)
+result = run("02TSFR", "1.Hj")
+print(result)
