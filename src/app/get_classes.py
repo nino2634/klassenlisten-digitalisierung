cc3a89d9f943f49be8cd95232ee848341367dca7
nino2634 <niklas.noechel@gmx.de>
2025-12-19 11:20:35 +0100

    comments

--- src/app/get_classes.py
+++ src/app/get_classes.py
@@ -3,7 +3,14 @@ import json
 from .config.config_handler import load_config_data
 from .get_headers import run as get_headers
 
-def run(class_filter=""):
+def run(class_filter):
+    """
+    Sucht in Spalte 1 des Worksheets nach Klassennamen, die den class_filter enthalten
+    
+    :param class_filter: String, Filter für Klassennamen
+    
+    returns: JSON-encoded Liste der gefundenen Klassennamen oder eine Fehlermeldung
+    """
     file=load_config_data("excel_file") 
     # Pandas DataFrame laden
     df = pd.read_excel(file, header=None)
@@ -11,14 +18,13 @@ def run(class_filter=""):
     # Erste Spalte extrahieren
     first_col = df.iloc[:, 0]
 
-    # Header-Liste aus deinem Helper
+    # Header-Liste laden
     headers = json.loads(get_headers())
 
-    # Filtern: nur Strings, Filter enthalten, nicht in headers
     classes = (
-        first_col[first_col.apply(lambda x: isinstance(x, str))]
-        .loc[lambda s: s.str.contains(class_filter)]
-        .loc[lambda s: ~s.str.strip().isin(headers)]
+        first_col[first_col.apply(lambda x: isinstance(x, str))] #nur strings
+        .loc[lambda s: s.str.contains(class_filter)] #filter prüfen
+        .loc[lambda s: ~s.str.strip().isin(headers)] #nicht in headers
         .tolist()
     )
 
