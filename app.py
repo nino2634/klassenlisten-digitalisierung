0e16aa8ef59cd45f22cc9b807a380109cf4f5ffb
Flo722 <47476412+Flo722@users.noreply.github.com>
2025-12-28 19:24:26 +0100

    fixed again

--- app.py
+++ app.py
@@ -110,9 +110,9 @@ def get_school_classes():
 @app.route("/api/load_progress",methods=["POST"])
 @login_required  
 def load_progress():
-    #progress_handler.check_and_reset()
+    progress_handler.check_and_reset()
     data = request.get_json()
-    term = data.get()
+    term = data.get("savedHalfYear")
     print(term)
 
     if not term:
